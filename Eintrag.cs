1ce6ca1997fe27fd8dc09e1427c5dcd0b5fb19f1
LuSc15 <luca.schmieding@gmail.com>
2024-11-06 15:55:19 +0100

    Projektdateien hinzufügen.

---
+++ Eintrag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_04_Durchschnitt_von_Kategorien
+{
+    internal class Eintrag
+    {
+        public string name {  get; set; }
+        public decimal price { get; set; }
+        public string kategorie { get; set; }
+        public Eintrag(string name,decimal price,string kategorie)
+        {
+            this.name = name;
+            this.price = price;
+            this.kategorie = kategorie;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LINQ_04_Durchschnitt_von_Kategorien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Eintrag> list = new List<Eintrag>();
+            string[] sammler;
+
+            using (StreamReader sr = new StreamReader(@"C:\Users\ITA5-TN13\source\repos\LuSc15\LINQ 04 Durchschnitt von Kategorien\Produkte.txt"))
+            {
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sammler = line.Replace('.', ',').Trim().Split(';');
+                    Eintrag e = new Eintrag(sammler[0], Convert.ToDecimal(sammler[1]), sammler[2]);
+                    list.Add(e);
+                }
+                sr.Close();
+            }
+         
+            foreach(Eintrag e in list)
+            {
+                Console.WriteLine(e.name+" "+e.price+" "+e.kategorie);
+            }
+               
+        }
+    }
+}
